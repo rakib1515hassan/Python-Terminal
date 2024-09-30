f364c560de4b61b32f9fd2dd15a6df35fdb74e31
Abu Ubaida <rakib1515hassan@gmail.com>
2024-09-30 09:14:18 +0600

    Update

--- table.py
+++ table.py
@@ -1,18 +1,23 @@
 from rich.table import Table
-
 from rich.console import Console
-
 
 console = Console()
 
 table = Table(title="User Data")
 
+data = {
+    0: {"id": 1, "name": "Rakib", "age": 28},
+    1: {"id": 2, "name": "Hassan", "age": 24},
+    2: {"id": 3, "name": "Tuhin", "age": 25},
+    3: {"id": 4, "name": "Rubel", "age": 22},
+    4: {"id": 5, "name": "Joshim", "age": 29},
+}
+
 table.add_column("ID", justify="right", style="cyan", no_wrap=True)
 table.add_column("Name", style="magenta")
 table.add_column("Age", justify="right", style="green")
 
-table.add_row("1", "Alice", "28")
-table.add_row("2", "Bob", "32")
-table.add_row("3", "Charlie", "22")
+for v in data.values():
+    table.add_row(str(v["id"]), v["name"], str(v["age"]))
 
 console.print(table)
